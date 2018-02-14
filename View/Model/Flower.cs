312228faf767e4699faa57a9e6ea13a83743e0de
miguelito657 <35902662+miguelito657@users.noreply.github.com>
2018-02-13 19:09:16 -0500

    First Upload

---
+++ View/Model/Flower.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Flower
+    {
+
+        // Window position
+        private int windowPosition;
+
+        // Adaptation level.
+        private double adaptation;
+
+
+        // We create the Flower's Chromosome
+
+        //Index's:
+        // 0: height
+        // 1: Flower's Color
+        // 2: Stem's Color
+        // 3: Stem's Width
+        // 4: Stem's Size
+        // 5: Size of Flower's Center.
+
+
+        public int[] chromosome = new int[6];
+
+        public int WindowPosition { get => windowPosition; set => windowPosition = value; }
+        public double Adaptation { get => adaptation; set => adaptation = value; }
+
+        public Flower()
+        {
+            // 
+            // TODO
+            // 
+        }
+    }
+}
+
---
+++ View/Model/Garden.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Garden
+    {
+        private Flower[] flowers;
+        private int generation;
+        private int iParentA;
+        private int iParentB;
+
+        public Garden()
+        {
+            Random random = new Random(unchecked((int)DateTime.Now.Ticks));
+            for (int n = 0; n < 10; n++)
+            {
+                Flower temp = new Flower();
+                flowers[n] = temp;
+                flowers[n].WindowPosition = n * 80 + 40;
+
+                // Inicializamos el cromosoma con valores al azar 
+
+                // La altura va de 10 a 300 
+                flowers[n].chromosome[0] = random.Next(10, 300);
+
+                // El color de la flor. 0-rojo, 1-azul, 2-amarillo 
+                flowers[n].chromosome[1] = random.Next(0, 3);
+
+                // El color del tallo. Diferentes tonos de verde 
+                flowers[n].chromosome[2] = random.Next(0, 3);
+
+                // El ancho del tallo. De 5 a 15 
+                flowers[n].chromosome[3] = random.Next(5, 15);
+
+                // El tamano de la flor. De 20 a 80 
+                flowers[n].chromosome[4] = random.Next(20, 80);
+
+                // El tamano del centro de la flor. De 5 a 15 
+                flowers[n].chromosome[5] = random.Next(5, 15);
+            }
+        }
+
+    }
+} 
+    
+
